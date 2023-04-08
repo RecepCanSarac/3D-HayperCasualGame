80e4678b125bf1d33bea7decd7f9975724fbc068
Recep Can Saraç <113214739+RecepCanSarac@users.noreply.github.com>
2023-04-09 00:00:39 +0300

    Added Game Manager

---
+++ HayperCasualGame3D/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject losePanel;
+    public int Score;
+    public Text scoreText;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void LevelEnd()
+    {
+        losePanel.SetActive(true);
+        scoreText.text = " Toplam Puan " + Score;
+    }
+}
